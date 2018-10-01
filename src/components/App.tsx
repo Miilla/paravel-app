8f1e8777eae5b135226357b16a7ad92517efccc5
Milena Moniszka <balletmila@gmail.com>
2018-10-01 23:14:33 +0200

    sample header added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-
-import logo from './logo.svg';
+import Header from './header/Header';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header/>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
